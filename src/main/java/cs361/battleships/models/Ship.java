5455986d1087627c25555245cd08bd6761553710
XIZHI YANG <35322574+yangxizhi@users.noreply.github.com>
2018-10-10 13:23:48 -0700

    fix the bug

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -3,8 +3,12 @@ package cs361.battleships.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Ship {
+	public Ship() {
+		occupiedSquares = new ArrayList<>();
+	}
 
 	@JsonProperty private List<Square> occupiedSquares;
 	
