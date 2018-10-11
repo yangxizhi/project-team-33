339079a73cead3d5a1a6114013bd304a30077963
XIZHI YANG <35322574+yangxizhi@users.noreply.github.com>
2018-10-11 12:09:33 -0700

    This is my first version of ship file.

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -3,17 +3,29 @@ package cs361.battleships.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Ship {
+	public Ship() {
+		occupiedSquares = new ArrayList<>();
+	}
 
 	@JsonProperty private List<Square> occupiedSquares;
-	
+
 	public Ship(String kind) {
-		//TODO implement
+		//Set the occupied squares of the ship based on its type.
+		if(kind.equals("Minesweeper")){
+			occupiedSquares = new ArrayList<>(2);
+		}else if(kind.equals("Destroyer")){
+			occupiedSquares = new ArrayList<>(3);
+		}else if(kind.equals("Battleship")){
+			occupiedSquares = new ArrayList<>(4);
+		}
+
 	}
 
 	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
+		//return the occupied squares of the ship.
+		return occupiedSquares;
 	}
 }
