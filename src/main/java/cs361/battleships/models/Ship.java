af3968bf90f77d677f9b5ef52430b34e49209edc
guilloca <32156785+guilloca@users.noreply.github.com>
2018-10-12 03:08:11 -0700

    Updated for vars being added

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -10,18 +10,45 @@ public class Ship {
 		occupiedSquares = new ArrayList<>();
 	}
 
+	private int length; // length of ship
+	private String type; // type of ship
+	private int hits; // num of hits
+
 	@JsonProperty private List<Square> occupiedSquares;
 
 	public Ship(String kind) {
 		//Set the occupied squares of the ship based on its type.
 		if(kind.equals("MINESWEEPER")){
 			occupiedSquares = new ArrayList<>(2);
+
 		}else if(kind.equals("DESTROYER")){
 			occupiedSquares = new ArrayList<>(3);
+
 		}else if(kind.equals("BATTLESHIP")){
 			occupiedSquares = new ArrayList<>(4);
-		}
 
+		}
+	}
+
+	public static int getLength(){
+		return this.length;
+	}
+	public void setLength(int aLength){
+		this.length = aLength;
+	}
+
+	public static int getType(){
+		return this.type;
+	}
+	public void setType(String aString){
+		this.type = aString;
+	}
+
+	public static int getHits(){
+		return this.hits;
+	}
+	public void setHits(int aHits;){
+		this.hits = aHits;
 	}
 
 	public List<Square> getOccupiedSquares() {
